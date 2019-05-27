65249208072b0b139a90b16889528690c2915863
htbkoo <h1017760@connect.hku.hk>
2019-05-27 19:26:41 +0800

    extracted to common PROD code block (to prepare for productionizing it)

--- src/engine/redux/todo/store/spikeRedux.test.ts
+++ src/engine/redux/todo/store/spikeRedux.test.ts
@@ -1,57 +1,61 @@
 import {createStore} from "redux";
 
 describe("redux spike", function () {
+    // PROD
+
+    interface Item {
+        message: string
+    }
+
+    interface State {
+        items: Item[]
+    }
+
+    enum ActionType {
+        ADD_ITEM, CLEAR_ITEM
+    }
+
+    interface AddItemAction {
+        type: ActionType.ADD_ITEM,
+        item: Item
+    }
+
+    interface ClearItemsAction {
+        type: ActionType.CLEAR_ITEM
+    }
+
+    type Action =
+        | AddItemAction
+        | ClearItemsAction;
+
+    function addItem(message: string): AddItemAction {
+        return {
+            type: ActionType.ADD_ITEM,
+            item: {message}
+        }
+    }
+
+    const reducer = function (state, action: Action): State {
+        switch (action.type) {
+            case ActionType.ADD_ITEM: {
+                return {items: [...state.items, action.item]}
+            }
+            default: {
+                return state;
+            }
+        }
+    };
+
+    type ActionCreator = (any) => Action;
+
+    //
+
     it("should create store", () => {
         // given
-        interface Item {
-            message: string
-        }
-
-        interface State {
-            items: Item[]
-        }
-
-        enum ActionType {
-            ADD_ITEM, CLEAR_ITEM
-        }
-
-        interface AddItemAction {
-            type: ActionType.ADD_ITEM,
-            item: Item
-        }
-
-        interface ClearItemsAction {
-            type: ActionType.CLEAR_ITEM
-        }
-
-        type Action =
-            | AddItemAction
-            | ClearItemsAction;
-
-        function addItem(message: string): AddItemAction {
-            return {
-                type: ActionType.ADD_ITEM,
-                item: {message}
-            }
-        }
-
-        const initState: State = {
-            items: []
-        };
-
-        const reducer = function (state = initState, action: Action): State {
-            switch (action.type) {
-                case ActionType.ADD_ITEM: {
-                    return {items: [...state.items, action.item]}
-                }
-                default: {
-                    return state;
-                }
-            }
-        };
+        const initState: State = {items: []};
 
         // when
-        const store = createStore(reducer,);
+        const store = createStore(reducer, initState);
         expect(store.getState()).toEqual(initState);
 
         // then
