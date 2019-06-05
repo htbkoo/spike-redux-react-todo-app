4f2a47be83ee5c651b48c0c49f3f24bda83271f3
htbkoo <h1017760@connect.hku.hk>
2019-06-05 20:55:46 +0800

    implemented simple UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
+import {connect} from 'react-redux';
+
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {State} from "./engine/redux/todo/models/common";
+import {addItem, clearItems, editItem} from "./engine/redux/todo/actions/ActionCreators";
+
+const mapStateToProps = ({items, itemById,}: State /*, ownProps*/) => {
+    return {
+        items,
+        itemById
+    }
 };
 
-export default App;
+const mapDispatchToProps = {addItem, clearItems, editItem};
+
+interface AppProps {
+    items: State["items"],
+    itemById: State["itemById"],
+    addItem: typeof addItem,
+    clearItems: typeof clearItems,
+    editItem: typeof editItem,
+}
+
+const App: React.FC<AppProps> = props => {
+    return (
+        <div className="App">
+            <div>
+                Simple react-redux todo list
+            </div>
+            <div style={{display: "flex", flexDirection: "column", marginLeft: "20%", marginRight: "20%",}}>
+                {props.items.map(id => (
+                        <input
+                            key={id}
+                            onChange={event => props.editItem(id, event.target.value)}
+                            value={props.itemById[id].message}
+                        />
+                    )
+                )}
+            </div>
+            <div>
+                <button onClick={() => props.addItem("")}>+</button>
+            </div>
+            <div>
+                <button onClick={props.clearItems}>Clear All</button>
+            </div>
+        </div>
+    );
+};
+
+export {App};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(App);
